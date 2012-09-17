fa745aa31db7506e936974bb405eb521913a1550
Ben Ford <bford@solaisecapital.com>
2012-09-17 15:36:06 +0100

    Load epy module individually for better control

--- personal/python.el
+++ personal/python.el
@@ -1,6 +1,12 @@
 (setq ropemacs-global-prefix "C-c p")
 (add-to-list 'load-path (concat prelude-vendor-dir "/epy"))
-(load "epy-init.el")
+;;(load "epy-init.el")
+(require 'epy-setup)      ;; It will setup other loads, it is required!
+(require 'epy-python)     ;; If you want the python facilities [optional]
+(require 'epy-completion) ;; If you want the autocompletion settings [optional]
+;;(require 'epy-editing)    ;; For configurations related to editing [optional]
+(require 'epy-bindings)   ;; For gabrielelanaro's suggested keybindings [optional]
+(require 'epy-nose)       ;; For nose integration
 (epy-setup-ipython)
 (epy-setup-checker "pyflakes %f")
 (setq virtualenv-workon-home "~/dev/ve")
